2cd8bf690ee9eec13337ab771175edd78191be5e
yiwereaminu <yiwereamin@gmail.com>
2023-12-17 19:59:58 +0000

    hero section styled

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,20 @@
 export default function Hero() {
   return (
-    <main className="container">
-      <img
-        className="hero-image"
-        src="/images/Group 77.png"
-        alt="main hero image"
-      ></img>
-      <div className="hero-content">
-        <h1 className="">Online Experiences</h1>
+    <main className="parent-container">
+      <div className="container">
+        <img
+          className="hero-image"
+          src="/images/Group 77.png"
+          alt="main hero image"
+        ></img>
+        <div className="hero-content">
+          <h1 className="">Online Experiences</h1>
 
-        <p>
-          Join unique interactive activities led by one-of-a-kind hosts—all
-          without leaving home.
-        </p>
+          <p>
+            Join unique interactive activities led by one-of-a-kind hosts—all
+            without leaving home.
+          </p>
+        </div>
       </div>
     </main>
   );
